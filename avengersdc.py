de38a420f2af72c1c0496821ef625bd8ae338372
icememory166 <darkheaven166@gmail.com>
2022-01-21 15:11:37 +0100

    Update avengersdc.py

--- avengersdc.py
+++ avengersdc.py
@@ -1,6 +1,6 @@
 import random
 
 avengers = 'Captain America Iron Man Thor Odinson Hulk Black Widow Hawkeye War Machine Vision Scarlet Witch Falcon Spider Man Ant Man Nebula Rocket'.split()
-heroes = 'Animal Man Aqua Man Bat Woman Blue Devil Cain Cyclone Dawn Star Enchantress Green Lantern Heat Wave Katana Mr Freeze Nekron Poison Ivy'
+heroes = 'Animal Man Aqua Man Bat Woman Blue Devil Cain Cyclone Dawn Star Enchantress Green Lantern Heat Wave Katana Mr Freeze Nekron Poison Ivy'.split()
 
 print(random.choice(avengers), random.choice(heroes))
