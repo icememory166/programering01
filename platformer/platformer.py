6c1fb20e70156775f741eabe69d072dd915f5304
icememory166 <darkheaven166@gmail.com>
2022-02-10 08:29:10 +0100

    upd

--- platformer/platformer.py
+++ platformer/platformer.py
@@ -1,9 +1,10 @@
-import pygame, sys, os
-
+import pygame, sys, os, random
 clock = pygame.time.Clock()
 
 from pygame.locals import *
+pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.init() # initiates pygame
+pygame.mixer.set_num_channels(64)
 
 pygame.display.set_caption('Pygame Platformer')
 
@@ -67,9 +68,19 @@ game_map = load_map('map')
 grass_img = pygame.image.load('grass.png')
 dirt_img = pygame.image.load('dirt.png')
 
+jump_sound = pygame.mixer.Sound('jump.wav')
+grass_sounds = [pygame.mixer.Sound('grass_0.wav'),pygame.mixer.Sound('grass_1.wav')]
+grass_sounds[0].set_volume(0.2)
+grass_sounds[1].set_volume(0.2)
+
+pygame.mixer.music.load('music.wav')
+pygame.mixer.music.play(-1)
+
 player_action = 'idle'
 player_frame = 0
 player_flip = False
+
+grass_sound_timer = 0
 
 player_rect = pygame.Rect(100,100,5,13)
 
@@ -107,6 +118,9 @@ def move(rect,movement,tiles):
 while True: # game loop
     display.fill((146,244,255)) # clear screen by filling it with blue
 
+    if grass_sound_timer > 0:
+        grass_sound_timer -= 1
+
     true_scroll[0] += (player_rect.x-true_scroll[0]-152)/20
     true_scroll[1] += (player_rect.y-true_scroll[1]-106)/20
     scroll = true_scroll.copy()
@@ -117,9 +131,9 @@ while True: # game loop
     for background_object in background_objects:
         obj_rect = pygame.Rect(background_object[1][0]-scroll[0]*background_object[0],background_object[1][1]-scroll[1]*background_object[0],background_object[1][2],background_object[1][3])
         if background_object[0] == 0.5:
-            pygame.draw.rect(display,(14,222,150),obj_rect)
+            pygame.draw.rect(display,(20,170,150),obj_rect)
         else:
-            pygame.draw.rect(display,(9,91,85),obj_rect)
+            pygame.draw.rect(display,(15,76,73),obj_rect)
 
     tile_rects = []
     y = 0
@@ -159,6 +173,10 @@ while True: # game loop
     if collisions['bottom'] == True:
         air_timer = 0
         vertical_momentum = 0
+        if player_movement[0] != 0:
+            if grass_sound_timer == 0:
+                grass_sound_timer = 30
+                random.choice(grass_sounds).play()
     else:
         air_timer += 1
 
@@ -175,12 +193,15 @@ while True: # game loop
             pygame.quit()
             sys.exit()
         if event.type == KEYDOWN:
+            if event.key == K_w:
+                pygame.mixer.music.fadeout(1000)
             if event.key == K_RIGHT:
                 moving_right = True
             if event.key == K_LEFT:
                 moving_left = True
             if event.key == K_UP:
                 if air_timer < 6:
+                    jump_sound.play()
                     vertical_momentum = -5
         if event.type == KEYUP:
             if event.key == K_RIGHT:
