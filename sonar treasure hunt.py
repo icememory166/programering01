948dd95d1f071b5f870ee359de0712a3c11d52c6
icememory166 <darkheaven166@gmail.com>
2022-02-23 12:31:53 +0100

    upd

--- sonar treasure hunt.py
+++ sonar treasure hunt.py
@@ -10,3 +10,12 @@ def getNewBoard():
     for x in range(60): # the main list is a list of 60 lists
         board.append([])
         for y in range(15): # each list in the main list has 15 single character strings
+            # use different characters for the ocean to make it more readable
+            if random.randint(0, 1) == 0:
+                board[x].append('~')
+            else:
+                board[x].append('`')
+    return board
+
+def drawBoard(board):
+                    
