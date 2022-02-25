ff2b77ed1d8d00ecb6ddfa0f76ba58227bff88c7
icememory166 <darkheaven166@gmail.com>
2022-02-25 15:13:10 +0100

    upd

--- cipher.py
+++ cipher.py
@@ -4,12 +4,12 @@ MAX_KEY_SIZE = len(SYMBOLS)
 
 def getMode():
     while True:
-        print('Do you wish to encrypt or decrypt a message?')
+        print('Do you wish to encrypt or decrypt or brute-force a message?')
         mode = input().lower()
         if mode in ['encrypt', 'e', 'decrypt', 'd']:
             return mode
         else:
-            print('Enter either "encrypt" or "e" or "decrypt" or "d".')
+            print('Enter either "encrypt" or "e" or "decrypt" or "d" or "brute" or "b".')
 
 def getMessage():
     print('Enter your message:')
@@ -30,4 +30,28 @@ def getTranslatedMessage(mode, message, key):
 
     for symbol in message:
         symbolIndex = SYMBOLS.find(symbol)
-        
+        if symbolIndex == -1: # Symbol not found in SYMBOLS
+            # just add this character without any change
+            translated += symbol
+
+            # encrypt or decrypt
+            symbolIndex += key
+
+            if symbolIndex >= len(SYMBOLS):
+                symbolIndex -= len(SYMBOLS)
+            elif symbolIndex < 0:
+                symbolIndex += len(SYMBOLS)
+
+            translated += SYMBOLS(symbolIndex)
+    return translated
+
+mode = getMode()
+message = getMessage()
+if mode[0] != 'b':
+    key = getKey()
+print('Your translated text is:')
+if mode[0] != 'b':
+    print(getTranslatedMessage(mode, message, key))
+else:
+    for key in range(1, MAX_KEY_SIZE + 1):
+        print(key, getTranslatedMessage('decrypt', message, key))    
