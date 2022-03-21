df3fccdd431bf23d5a7fe97979a3cffc972cacf7
icememory166 <darkheaven166@gmail.com>
2022-03-21 14:27:50 +0100

    upd

--- pygamehelloworld.py
+++ pygamehelloworld.py
@@ -14,3 +14,28 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+# set up the fonts
+basicFont = pygame.font.Sysfont(None, 48)
+
+# Set up the text
+text = basicFont.render('Hello World!', True, WHITE, BLUE)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery
+
+# draw the white background onto the surface
+windowSurface.fill(WHITE)
+
+# draw a green polygon onto the surface.
+pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
+
+# draw some blue lines onto the surface.
+pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
+pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
+pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
+
+# draw a blue circle onto the surface.
+pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
+
+# draw a red ellipse onto the surface.
