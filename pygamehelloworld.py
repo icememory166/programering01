a8f130183c771a8fb6763bddf0f61575c54905fa
icememory166 <darkheaven166@gmail.com>
2022-03-21 11:08:31 +0100

    upd

--- pygamehelloworld.py
+++ pygamehelloworld.py
@@ -3,3 +3,14 @@ from pygame.locals import *
 
 # set up pygame
 pygame.init()
+
+# Set up a window.
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('Hello World!')
+
+# Set up the colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
